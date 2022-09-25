c04ee90e44d51212b876b12daa929ee091bfa8da
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-09-25 18:55:13 +0200

    Fix type

--- reduction/mod_reduction.py
+++ reduction/mod_reduction.py
@@ -2,5 +2,5 @@
 from euclidian.int_euclidian import long_division
 
 
-def mod_reduction(radix: str, x: str, modulus: str) -> str:
+def mod_reduction(radix: int, x: str, modulus: str) -> str:
     return long_division(radix, x, modulus)[1]
