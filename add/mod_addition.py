e58647f3baeb5f6358cbd3bb84091a0282888733
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2022-09-25 20:35:11 +0200

    Let mod_addition use correct addition and reduction methods

--- add/mod_addition.py
+++ add/mod_addition.py
@@ -1,20 +1,19 @@
 
-from operator import mod #moet verwijderd worden als mod_reduction werkt
+from helpers import radix_to_decimal
+from reduction.mod_reduction import mod_reduction
 from int_addition import addition
 from subtraction.int_subtraction import subtraction
 
-def mod_addition(radix,x,y,m):
-    #x = mod_reduction(radix, x, m)
-    #y = mod_reduction(radix, y, m)
-    x = mod(int(x, radix), m)
-    y = mod(int(y, radix), m)
+
+def mod_addition(radix, x, y, m):
+    x = mod_reduction(radix, x, m)
+    y = mod_reduction(radix, y, m)
 
     if (y == "0"):
         return x
-    
-    z_prime = addition(radix, x, y)[0]
-    if (int(z_prime, radix) < int(m, radix)):
-        #return mod_reduction(radix, z_prime, m)
-        return mod(int(z_prime, radix), m)
-    
-    return subtraction(radix, z_prime, m)[0]
+
+    z_prime = addition(radix, x, y)
+    if (radix_to_decimal(radix, z_prime) < radix_to_decimal(radix, m)):
+        return mod_reduction(radix, z_prime, m)
+
+    return subtraction(radix, z_prime, m)
